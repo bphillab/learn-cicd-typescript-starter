0a77cac3977be57b67da0c5ded8595280ad54b52
benjaminphillabaum <bphillab@gmail.com>
2026-03-18 07:13:16 -0400

    fix code

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,12 +3,12 @@ import { IncomingHttpHeaders } from "http";
 export function getAPIKey(headers: IncomingHttpHeaders): string | null {
   const authHeader = headers["authorization"];
   if (!authHeader) {
-    return "";
+    return null;
   }
 
   const splitAuth = authHeader.split(" ");
   if (splitAuth.length < 2 || splitAuth[0] !== "ApiKey") {
-    return "";
+    return null
   }
 
   return splitAuth[1];
