1ef79bc25e089fc741f01659a9c8b21e3e5d6549
benjaminphillabaum <bphillab@gmail.com>
2026-03-18 07:43:50 -0400

    updated files for prettier

--- src/test/api.test.ts
+++ src/test/api.test.ts
@@ -1,35 +1,35 @@
 import { describe, expect, test } from "vitest";
-import {getAPIKey} from "../api/auth.js";
+import { getAPIKey } from "../api/auth.js";
 
 const person = {
-    isActive: true,
-    age: 32,
+  isActive: true,
+  age: 32,
 };
 
 describe("person", () => {
-    test("person is defined", () => {
-        expect(person).toBeDefined();
-    });
+  test("person is defined", () => {
+    expect(person).toBeDefined();
+  });
 
-    test("is active", () => {
-        expect(person.isActive).toBeTruthy();
-    });
+  test("is active", () => {
+    expect(person.isActive).toBeTruthy();
+  });
 });
 
 const headers1 = {
-    "authorization": "Bearer test-token",
-    "x-api-key": "test-key",
+  authorization: "Bearer test-token",
+  "x-api-key": "test-key",
 };
 const headers2 = {
-    "authorization": "ApiKey test-token",
+  authorization: "ApiKey test-token",
 };
 const headers3 = {
-    "authorization": "ApiKey",
+  authorization: "ApiKey",
 };
 describe("getAPIKey", () => {
-    test("returns the api key from the headers", () => {
-        expect(getAPIKey(headers1)).toBe(null)
-        expect(getAPIKey(headers2)).toBe("test-token")
-        expect(getAPIKey(headers3)).toBe(null)
-    })
-})
+  test("returns the api key from the headers", () => {
+    expect(getAPIKey(headers1)).toBe(null);
+    expect(getAPIKey(headers2)).toBe("test-token");
+    expect(getAPIKey(headers3)).toBe(null);
+  });
+});
